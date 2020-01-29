f8e8384582145e5da2d5c29d723774a00a2e8954
Robinson Souza <Robinsond76@gmail.com>
2020-01-29 18:39:12 -0500

    JSON Web token implemented

--- routers/users.js
+++ routers/users.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 //@route    POST api/users
 //@desc     Register a user
@@ -46,7 +49,23 @@ router.post(
 
       await user.save();
 
-      res.send('User saved');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 3600 // seconds before expiring
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
